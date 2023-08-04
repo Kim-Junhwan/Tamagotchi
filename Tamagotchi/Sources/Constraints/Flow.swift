abce16f5d6c0024e870e5580516602933aa57e7b
Kim junhwan <tazanboy1@naver.com>
2023-08-04 18:09:48 +0900

    [feat] 첫 실행 화면 분기처리

---
+++ Tamagotchi/Sources/Constraints/Flow.swift
@@ -0,0 +1,10 @@
+//
+//  Flow.swift
+//  Tamagotchi
+//
+//  Created by JunHwan Kim on 2023/08/04.
+//
+
+enum Flow {
+    static let startBranchCondition: String = "isLaunched"
+}
---
+++ Tamagotchi/Sources/Constraints/StoryBoard.swift
@@ -0,0 +1,15 @@
+//
+//  StoryBoard.swift
+//  Tamagotchi
+//
+//  Created by JunHwan Kim on 2023/08/04.
+//
+
+enum StoryBoard {
+    static let main: String = "Main"
+}
+
+enum ViewControllerIdenfier {
+    static let selectTama: String = "SelectTamagotchi"
+    static let detailTama: String = "DetailTamagotchi"
+}
