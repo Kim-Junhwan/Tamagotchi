42ef38326c23b25675cc59bbfe51327222da91c8
Kim junhwan <tazanboy1@naver.com>
2023-08-05 16:38:24 +0900

    [feat] 색상 정의

---
+++ Tamagotchi/Sources/Presentation/Utils/DefaultColor.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultColor.swift
+//  Tamagotchi
+//
+//  Created by JunHwan Kim on 2023/08/05.
+//
+
+import Foundation
+import UIKit
+
+enum DefaultColor {
+    static let defualtFontColor: UIColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
