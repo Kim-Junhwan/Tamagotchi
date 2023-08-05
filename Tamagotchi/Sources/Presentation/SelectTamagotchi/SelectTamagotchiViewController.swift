77b157dd9d5a0799bf8cd171164e1ba6eeb12660
Kim junhwan <tazanboy1@naver.com>
2023-08-05 19:02:01 +0900

    [feat] 콜렉션 뷰 UI 구성

--- Tamagotchi/Sources/Presentation/SelectTamagotchi/SelectTamagotchiViewController.swift
+++ Tamagotchi/Sources/Presentation/SelectTamagotchi/SelectTamagotchiViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class SelectTamagotchiViewController: UIViewController {
+    
+    private enum Metric {
+        static let cellWidth: Double = 110.0
+        static let cellHeight: Double = 130.0
+        static let spacing: Double = 15.0
+    }
 
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -15,13 +21,31 @@ class SelectTamagotchiViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        setCollectionView()
+    }
+    
+    private func makeCollectionViewLayout() -> UICollectionViewFlowLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = Metric.spacing
+        flowLayout.minimumInteritemSpacing = Metric.spacing
+        flowLayout.sectionInset = UIEdgeInsets(top: .zero, left: Metric.spacing, bottom: .zero, right: Metric.spacing)
+        flowLayout.itemSize = CGSize(width: Metric.cellWidth, height: Metric.cellHeight)
+        return flowLayout
     }
     
     private func setCollectionView() {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(UINib(nibName: SelectTamagotchiCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: SelectTamagotchiCollectionViewCell.identifier)
+        collectionView.collectionViewLayout = makeCollectionViewLayout()
     }
+    
+    
 
 }
 
@@ -33,7 +57,7 @@ extension SelectTamagotchiViewController: UICollectionViewDelegate, UICollection
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SelectTamagotchiCollectionViewCell.identifier, for: indexPath) as? SelectTamagotchiCollectionViewCell else { return UICollectionViewCell() }
-        
+        cell.configureCell(tamagotchi: tamagotchiList[indexPath.row])
         return cell
     }
 }
