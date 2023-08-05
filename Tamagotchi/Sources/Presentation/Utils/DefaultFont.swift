cc4ca629ba87ceed10f339c1b5b8c6630a42087b
Kim junhwan <tazanboy1@naver.com>
2023-08-05 18:56:11 +0900

    [refactor] 폰트 사이즈와 굵기 지정해서 반환 할 수 있도록 코드 수정

--- Tamagotchi/Sources/Presentation/Utils/DefaultFont.swift
+++ Tamagotchi/Sources/Presentation/Utils/DefaultFont.swift
@@ -9,18 +9,33 @@ import Foundation
 import UIKit
 
 enum DefaultFont {
-    case big
-    case medium
-    case small
     
-    var font: UIFont {
+    case system
+    case bold
+    
+    enum Size: Double {
+        case big
+        case medium
+        case small
+        
+        var size: Double {
+            switch self {
+            case .big:
+                return 15.0
+            case .medium:
+                return 14.0
+            case .small:
+                return 13.0
+            }
+        }
+    }
+    
+    func font(size: Size) -> UIFont {
         switch self {
-        case .big:
-            return UIFont.systemFont(ofSize: 15.0)
-        case .medium:
-            return UIFont.systemFont(ofSize: 14.0)
-        case .small:
-            return UIFont.systemFont(ofSize: 13.0)
+        case .system:
+            return UIFont.systemFont(ofSize: size.size)
+        case .bold:
+            return UIFont.boldSystemFont(ofSize: size.size)
         }
     }
 }
