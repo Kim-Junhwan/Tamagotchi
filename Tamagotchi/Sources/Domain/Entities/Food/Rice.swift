f0b76a152f8b7f959c8be2ec48c427e5b7593d16
Kim junhwan <tazanboy1@naver.com>
2023-08-05 14:07:53 +0900

    [feat] 먹이 추가

---
+++ Tamagotchi/Sources/Domain/Entities/Food/Rice.swift
@@ -0,0 +1,10 @@
+//
+//  Rice.swift
+//  Tamagotchi
+//
+//  Created by JunHwan Kim on 2023/08/04.
+//
+
+struct Rice: Eatable {
+    var exp: Exp = Exp(value: 0.2)
+}
---
+++ Tamagotchi/Sources/Domain/Entities/Food/WaterDrop.swift
@@ -0,0 +1,10 @@
+//
+//  WaterDrop.swift
+//  Tamagotchi
+//
+//  Created by JunHwan Kim on 2023/08/05.
+//
+
+struct WaterDrop: Eatable {
+    var exp: Exp = Exp(value: 0.5)
+}
