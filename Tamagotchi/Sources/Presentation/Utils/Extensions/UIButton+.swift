45011f8ccebfe36d2997853a464f74c0af3021b0
Kim junhwan <tazanboy1@naver.com>
2023-08-06 18:35:18 +0900

    [feat] UIButton Configuraion 이미지 패딩, 테두리 넣는 기능 추가

--- Tamagotchi/Sources/Presentation/Utils/Extensions/UIButton+.swift
+++ Tamagotchi/Sources/Presentation/Utils/Extensions/UIButton+.swift
@@ -11,17 +11,25 @@ extension UIButton {
     static func makeDefaultButtonConfiguration(backgroundColor: UIColor,
                                                font: UIFont,
                                                image: UIImage? = nil,
+                                               imagePadding: Double = 1.0,
                                                textColor: UIColor,
                                                title: String,
-                                               cornerRadius: Double) -> UIButton.Configuration
+                                               cornerRadius: Double,
+                                               borderWidth: Double = 0.0) -> UIButton.Configuration
     {
         var config = UIButton.Configuration.filled()
         var titleAttribute = AttributedString(title)
+        config.baseForegroundColor = textColor
         titleAttribute.font = font
-        titleAttribute.foregroundColor = textColor
         config.baseBackgroundColor = backgroundColor
         config.attributedTitle = titleAttribute
         config.background.cornerRadius = cornerRadius
+        config.background.strokeWidth = borderWidth
+        config.background.strokeColor = DefaultColor.defualtFontColor
+        if image != nil {
+            config.image = image
+            config.imagePadding = imagePadding
+        }
         return config
     }
 }
