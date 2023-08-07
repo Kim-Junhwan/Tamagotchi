2c53111fd054e3525eeb5a6b36ea66667242622f
Kim junhwan <tazanboy1@naver.com>
2023-08-07 16:56:59 +0900

    [feat] 밥, 물방울 Eatable 프로토콜 내용 구현

--- Tamagotchi/Sources/Domain/Entities/Food/Rice.swift
+++ Tamagotchi/Sources/Domain/Entities/Food/Rice.swift
@@ -6,5 +6,27 @@
 //
 
 struct Rice: Eatable {
+    
     var exp: Exp = Exp(value: 0.2)
+    
+    init() {}
+    
+    private init(exp: Exp) {
+        self.exp = exp
+    }
+    
+    static func eatingSeveral(num: Int) -> Rice {
+        return Rice(exp: num * Rice().exp)
+    }
+    
+    func eaten(num: Int) {
+        UserDefaultRepository.eatRiceCount += num
+    }
+    
+    func checkCanEat(num: Int) -> Bool {
+        if num >= 100 {
+            return false
+        }
+        return true
+    }
 }
--- Tamagotchi/Sources/Domain/Entities/Food/WaterDrop.swift
+++ Tamagotchi/Sources/Domain/Entities/Food/WaterDrop.swift
@@ -6,5 +6,27 @@
 //
 
 struct WaterDrop: Eatable {
+    
     var exp: Exp = Exp(value: 0.5)
+    
+    init() {}
+    
+    private init(exp: Exp) {
+        self.exp = exp
+    }
+    
+    static func eatingSeveral(num: Int) -> WaterDrop {
+        return WaterDrop(exp: num * WaterDrop().exp)
+    }
+    
+    func eaten(num: Int) {
+        UserDefaultRepository.eatWaterDropCount += num
+    }
+    
+    func checkCanEat(num: Int) -> Bool {
+        if num >= 50 {
+            return false
+        }
+        return true
+    }
 }
