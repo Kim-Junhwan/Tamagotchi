72686c66ba3f2001c4e24833867c52ca6e70c28e
Kim junhwan <tazanboy1@naver.com>
2023-08-07 16:57:49 +0900

    [feat] 레벨 로직 구현

--- Tamagotchi/Sources/Domain/Entities/Status/Level.swift
+++ Tamagotchi/Sources/Domain/Entities/Status/Level.swift
@@ -6,9 +6,42 @@
 //
 
 struct Level {
-    let value: Int
+    private let _value: Exp
+    
+    var value: Int {
+        get {
+            return levelLogic(exp: _value)
+        }
+    }
     
     init(exp: Exp) {
-        value = Int(exp.value)
+        _value = exp
+    }
+    
+    private func levelLogic(exp: Exp) -> Int {
+        switch exp.value {
+        case 0..<20 :
+            return 1
+        case 20..<30:
+            return 2
+        case 30..<40:
+            return 3
+        case 40..<50:
+            return 4
+        case 50..<60:
+            return 5
+        case 60..<70:
+            return 6
+        case 70..<80:
+            return 7
+        case 80..<90:
+            return 8
+        case 90..<100:
+            return 9
+        case 100...:
+            return 10
+        default:
+            return 1
+        }
     }
 }
