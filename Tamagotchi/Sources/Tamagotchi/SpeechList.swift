9e555751bbc62a00e8cef96f088f875ca34bddda
Kim junhwan <tazanboy1@naver.com>
2023-08-07 22:18:39 +0900

    [refactor] 다마고치 이야기 유저 이름을 포함하도록 수정

--- Tamagotchi/Sources/Tamagotchi/SpeechList.swift
+++ Tamagotchi/Sources/Tamagotchi/SpeechList.swift
@@ -9,14 +9,16 @@ import Foundation
 
 struct SpeechList {
     let userName: String
-    private var speechList: [String] = ["반갑습니다.", "고양이","강아지","기린","토끼","거북이","햄스터"]
+    
+    private lazy var speechList: [String] = ["\(userName)님 반가워요", "고양이를 좋아하시나요 \(userName)님?","오늘 하루도 힘내세요 \(userName)님","\(userName)님은 어떤걸 좋아하시나요?","\(userName)님\(userName)님\(userName)님\(userName)님\(userName)님","오늘 하루도 수고많았습니다 \(userName)님","\(userName)님 행복한 하루 되세요"]
+    
+    var currentSpeech: String {
+        mutating get {
+            speechList.randomElement() ?? "반가워요 \(userName)님"
+        }
+    }
     
     init(userName: String) {
         self.userName = userName
-    }
-    
-    func getRandomSpeech() -> String {
-        guard let speech = speechList.randomElement() else { return "" }
-        return speech
     }
 }
